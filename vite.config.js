e7cc908f06360be8765d9fb2205d2ebba0b1431f
neurvinch <npandian515@gmail.com>
2026-02-27 08:38:15 +0530

    the major rect reverted back to ui fix

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwind from "@tailwindcss/vite"
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react(), tailwind()],
-})
